6f021a4fb93edbc63b21cbf342cdc827a93d3e8a
Ahmad2025755 <ahmad.alain2022@gmail.com>
2025-07-17 19:09:12 +0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,12 @@
+function SumofTwoNumbers(){
+    var x , y , z
+    x = 6
+    y = 7
+    z = x + y
+
+
+    return z;
+}
+
+var sum =  SumofTwoNumbers()
+console.log('The sum of both numbers is ' + sum);
